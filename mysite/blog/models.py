9c8f19dde150bfebbcd939a734b4da8eb0186cb2
bydhorizon0 <bydhorizon0@gmail.com>
2026-01-07 00:35:36 +0900

    - url 접근 변경 - pagination 적용

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = ("PB", "Published")
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -37,3 +38,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        publish_kor = timezone.localtime(self.publish)
+        return reverse(
+            "blog:post_detail",
+            args=[publish_kor.year, publish_kor.month, publish_kor.day, self.slug],
+        )
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,34 @@
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    # 페이지당 3개의 게시물로 페이지 매김
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.get_page(page_number)  # paginator.page()는 strict한 방식이라 예외처리 필수
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request: HttpRequest, id: int) -> HttpResponse:
-    post = get_object_or_404(Post, pk=id)
-    return render(request, "blog/post/detail.html", {"post": post})
+def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str) -> HttpResponse:
+    post_obj = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(request, "blog/post/detail.html", {"post": post_obj})
