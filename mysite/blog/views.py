810996280c54b08263092d64c0e3e1871da2960b
bydhorizon0 <bydhorizon0@gmail.com>
2026-01-12 01:33:49 +0900

    django-taggit & 커스텀 템플릿 태그 및 필 터

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,11 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
+from django.db.models.aggregates import Count
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from blog.forms import EmailForm, CommentForm
 from blog.models import Post
@@ -16,13 +18,17 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request: HttpRequest) -> HttpResponse:
-    post_list = Post.objects.all()
+def post_list(request: HttpRequest, tag_slug=None) -> HttpResponse:
+    all_posts = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        all_posts = all_posts.filter(tags__in=[tag])
     # 페이지당 3개의 게시물로 페이지 매김
-    paginator = Paginator(post_list, 3)
+    paginator = Paginator(all_posts, 3)
     page_number = request.GET.get("page", 1)
     posts = paginator.get_page(page_number)  # paginator.page()는 strict한 방식이라 예외처리 필수
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str) -> HttpResponse:
@@ -35,9 +41,18 @@ def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str
         publish__day=day,
     )
     comments = post_obj.comments.all()
+    # 사용자가 댓글을 달 수 있는 폼
     form = CommentForm()
+    # 유사한 게시물들의 목록
+    post_tags_ids = post_obj.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post_obj.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
     return render(
-        request, "blog/post/detail.html", {"post": post_obj, "comments": comments, "form": form}
+        request,
+        "blog/post/detail.html",
+        {"post": post_obj, "comments": comments, "form": form, "similar_posts": similar_posts},
     )
 
 
